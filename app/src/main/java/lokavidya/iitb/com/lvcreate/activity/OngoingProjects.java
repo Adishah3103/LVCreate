ce4da846f2af3c263208668f2dc68ce329f5d10c
Antariksh C <antarikshc@gmail.com>
2018-07-16 04:49:25 +0530

    Redesigned and Recoded this whole thing
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/lokavidya/iitb/com/lvcreate/activity/OngoingProjects.java
+++ app/src/main/java/lokavidya/iitb/com/lvcreate/activity/OngoingProjects.java
@@ -1,25 +1,20 @@
 package lokavidya.iitb.com.lvcreate.activity;
 
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import lokavidya.iitb.com.lvcreate.R;
-
 import lokavidya.iitb.com.lvcreate.adapter.ProjectListAdapter;
-import lokavidya.iitb.com.lvcreate.model.HomeItem;
-import lokavidya.iitb.com.lvcreate.model.ProjectList;
+import lokavidya.iitb.com.lvcreate.dbUtils.ProjectDb;
+import lokavidya.iitb.com.lvcreate.model.Project;
 
 public class OngoingProjects extends AppCompatActivity {
 
@@ -29,6 +24,8 @@ public class OngoingProjects extends AppCompatActivity {
     RecyclerView projectlist;
     ProjectListAdapter adapter;
     View rootLayout;
+
+    ProjectDb mDb;
 
 
     @Override
@@ -45,22 +42,18 @@ public class OngoingProjects extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        projectlist =  (RecyclerView)findViewById(R.id.projectlist_recycler);
-        ArrayList<ProjectList> data = new ArrayList<>();
-        data.add(new ProjectList("Title of Video 1", " null", "2:30"));
-        data.add(new ProjectList("Title of Video 2", "null", "3:50"));
-        data.add(new ProjectList("Title of Video 3", "null", "4:10"));
-        data.add(new ProjectList("Title of Video 4", "null", "6:30"));
-        data.add(new ProjectList("Title of Video 5", "null", "2:40"));
-        data.add(new ProjectList("Title of Video 6", "null", "3:15"));
+        projectlist = findViewById(R.id.projectlist_recycler);
+
+        // Get database instance
+        mDb = ProjectDb.getsInstance(getApplicationContext());
+
+        List<Project> data = mDb.projectDao().loadAllProject();
 
         adapter = new ProjectListAdapter(data);
         projectlist.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
         projectlist.setAdapter(adapter);
 
-
-
-
+        adapter.notifyDataSetChanged();
 
     }
 
