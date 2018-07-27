b381e4ef561949f35b7009ccd35354459291782b
Antariksh C <antarikshc@gmail.com>
2018-07-27 14:41:07 +0530

    Decrease Splash timeout
    Signed-off-by: Antariksh C <antarikshc@gmail.com>

--- app/src/main/java/lokavidya/iitb/com/lvcreate/activity/SplashScreen.java
+++ app/src/main/java/lokavidya/iitb/com/lvcreate/activity/SplashScreen.java
@@ -10,7 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import lokavidya.iitb.com.lvcreate.R;
 
 public class SplashScreen extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 1500;
+    private static int SPLASH_TIME_OUT = 400;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
